8be8bc34f2a08f5b14c60a2cf800c46f8f5767ae
Guilherme Xavier <131989419+Xavier4290@users.noreply.github.com>
2025-11-27 22:01:23 -0300

    Add files via upload

---
+++ TCC_v0.01.py
@@ -0,0 +1,35 @@
+#Bibliotecas
+
+#def conex_bd() Gustavo e Xavier
+#Função com a responsabilidade de se conectar com a base de dados e alimentar e receber informações
+
+
+#def conex_raspberry() Xavier e Enzo
+#Função que deverá ser capaz de se conectar com o pluviometro e enviar instruções e receber suas informações
+
+
+#def alg_adaptativo() Gustavo
+#Algoritimo responsável por determinar a frequência de coletas em períodos de estiagem ou de maior frequencia de chuvas
+
+
+#def ia() Gustavo e Maicon
+#Função que hospedará a IA que fará análises simples das informações contidas na base de dados, ou que forem requisitadas do pluviometro
+
+
+#def interface() Xavier
+#Função simples para o front-end
+
+
+#def alertas() Gustavo
+#Função responsável por emitir as alertas baseado no volume de chuva
+
+
+#def calculos() Gustavo
+#Função que hospedará todos os cálculos necessários para alimentar as demais funções
+
+
+#def conexão com PowerBI(?)
+#Incognita se realmente necessária ou não, deveria ser responsável por atualizar as informações no PowerBI
+
+
+#def main()
